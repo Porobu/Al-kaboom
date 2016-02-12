4bc4036641aecfa46ce365d1ce355efdd9b96a41
Sergio <porobu@gmail.com>
2016-02-12 18:50:18 +0100

    UI Guapa

--- Al-kaboom/src/si/alkaboom/backend/Nagusia.java
+++ Al-kaboom/src/si/alkaboom/backend/Nagusia.java
@@ -1,9 +1,20 @@
 package si.alkaboom.backend;
 
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 public class Nagusia {
 	public static final float BERTSIOA = 0.0F;
+
 	public static void main(String[] args) {
-		
+		if (!System.getProperty("os.name").contains("OS X"))
+			try {
+				UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+			} catch (ClassNotFoundException | InstantiationException | IllegalAccessException
+					| UnsupportedLookAndFeelException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 	}
 
 }

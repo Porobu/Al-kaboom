d5fb360749bbfcadecc4a1c87892748f7cd826ef
Sergio <porobu@gmail.com>
2016-02-23 13:37:16 +0100

    ya tenemos algo

--- Al-kaboom/src/si/alkaboom/frontend/NagusiaUI.java
+++ Al-kaboom/src/si/alkaboom/frontend/NagusiaUI.java
@@ -1,8 +1,16 @@
 package si.alkaboom.frontend;
 
+import java.awt.BorderLayout;
+
 import javax.swing.JFrame;
 
 public class NagusiaUI extends JFrame {
 	private static final long serialVersionUID = 6902848848036097903L;
-
+	public NagusiaUI() {
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLayout(new BorderLayout());
+		this.setTitle("Al-Kaboom");
+		this.pack();
+		this.setVisible(true);
+	}
 }

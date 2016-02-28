2a0e117dabb209a9fd35b8f8089b1a06d06cfed9
Sergio <porobu@gmail.com>
2016-02-28 18:56:27 +0100

    konstante hobeak

--- Al-kaboom/src/si/alkaboom/backend/AlKaboomConstants.java
+++ Al-kaboom/src/si/alkaboom/backend/AlKaboomConstants.java
@@ -1,9 +1,20 @@
 package si.alkaboom.backend;
 
 public interface AlKaboomConstants {
-	public static final float BERTSIOA = 0.1F;
+	public static final double BERTSIOA = 0.1;
 	public static final String IZENBURUA = "Al-Kaboom " + BERTSIOA;
-	public static final int[] ERREZA = new int[] { 0, 0, 0 };
-	public static final int[] NORMALA = new int[] { 0, 0, 0 };
-	public static final int[] ZAILA = new int[] { 0, 0, 0 };
+	public static final int ERRENKADA_KOPURUA_ERREZA = 0;
+	public static final int ZUTABE_KOPURUA_ERREZA = 0;
+	public static final int MINA_KOPURUA_ERREZA = 0;
+	public static final int ERRENKADA_KOPURUA_NORMALA = 0;
+	public static final int ZUTABE_KOPURUA_NORMALA = 0;
+	public static final int MINA_KOPURUA_NORMALA = 0;
+	public static final int ERRENKADA_KOPURUA_ZAILA = 0;
+	public static final int ZUTABE_KOPURUA_ZAILA = 0;
+	public static final int MINA_KOPURUA_ZAILA = 0;
+	public static final int[] ERREZA = new int[] { ERRENKADA_KOPURUA_ERREZA, ZUTABE_KOPURUA_ERREZA,
+			MINA_KOPURUA_ERREZA };
+	public static final int[] NORMALA = new int[] { ERRENKADA_KOPURUA_NORMALA, ZUTABE_KOPURUA_NORMALA,
+			MINA_KOPURUA_NORMALA };
+	public static final int[] ZAILA = new int[] { ERRENKADA_KOPURUA_ZAILA, ZUTABE_KOPURUA_ZAILA, MINA_KOPURUA_ZAILA };
 }

067d85f97199dd2a893bf27edb871d6700681ef8
aortuondo002 <aortuondo002@ikasle.ehu.es>
2016-04-05 12:47:10 +0200

    Sergio:"Soy malvado y lo vas a escribir"
    Hasierako tableroa sortzen duen algoritmoa da.
    
    Oraidik Laukia barruan dauden klaseak bete behar dira

--- Al-kaboom/src/si/alkaboom/backend/TableroModeloa.java
+++ Al-kaboom/src/si/alkaboom/backend/TableroModeloa.java
@@ -1,11 +1,56 @@
 package si.alkaboom.backend;
 
+import java.util.Random;
+
+import si.alkaboom.backend.laukia.ILaukia;
+import si.alkaboom.backend.laukia.LaukiaHutsa;
+import si.alkaboom.backend.laukia.LaukiaMina;
+import si.alkaboom.backend.laukia.LaukiaZenb;
+
 public class TableroModeloa {
-	
+	private ILaukia[][] tableroa;
+
 	public TableroModeloa(int errenkadak, int zutabeak, int minak, int klikErrenkada, int klikZutabea) {
-		
+		tableroa = new ILaukia[errenkadak][zutabeak];
+		tableroa[klikErrenkada][klikZutabea] = new LaukiaHutsa();
+		Random r = new Random();
+		for (int i = 0; i < minak; i++) {
+			int a = r.nextInt(errenkadak), b = r.nextInt(zutabeak);
+			if (tableroa[a][b] == null) {
+				tableroa[a][b] = new LaukiaMina();
+			} else {
+				i--;
+			}
+		}
+		for (int i = 0; i < errenkadak; i++) {
+			for (int j = 0; j < zutabeak; j++) {
+				if (tableroa[i][j].getClass().getName().contains("Mina")) {
+					zenbakiagehitu(i, j);
+				}
+
+			}
+		}
+		for (int i = 0; i < errenkadak; i++) {
+			for (int j = 0; j < zutabeak; j++) {
+				if (tableroa[i][j] == null) {
+					tableroa[i][j] = new LaukiaHutsa();
+				}
+			}
+		}
 	}
 
-	
-	
+	private void zenbakiagehitu(int errenkada, int zutabea) {
+		for (int i = -1; i <2; i++) {
+			for (int j = -1; j < 2; j++) {
+				if(tableroa[errenkada+i][zutabea+j]==null){
+					tableroa[errenkada+i][zutabea+j]=new LaukiaZenb();
+				}
+				else if(tableroa[errenkada+i][zutabea+j].getClass().getName().contains("Zenb")){
+					LaukiaZenb lz=(LaukiaZenb) tableroa[errenkada+i][zutabea+j];
+					lz.zenbPlus();
+				}
+			}
+		}
+	}
+
 }
--- Al-kaboom/src/si/alkaboom/backend/laukia/LaukiaZenb.java
+++ Al-kaboom/src/si/alkaboom/backend/laukia/LaukiaZenb.java
@@ -2,4 +2,19 @@ package si.alkaboom.backend.laukia;
 
 public class LaukiaZenb implements ILaukia {
 
+		private int zenbakia;
+		
+		public LaukiaZenb(){
+			zenbakia=1;
+		}
+
+		public int getZenbakia() {
+			return zenbakia;
+		}
+
+		public void zenbPlus(){
+			zenbakia++;
+		}
+		
+		
 }

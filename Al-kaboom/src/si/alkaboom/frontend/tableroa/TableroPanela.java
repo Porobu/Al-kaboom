73d90ac438c847b355bec2da707cb46130bb3eae
Sergio <porobu@gmail.com>
2016-04-08 10:45:25 +0200

    algo

--- Al-kaboom/src/si/alkaboom/frontend/tableroa/TableroPanela.java
+++ Al-kaboom/src/si/alkaboom/frontend/tableroa/TableroPanela.java
@@ -8,6 +8,7 @@ import si.alkaboom.backend.AlKaboomConstants;
 
 public class TableroPanela extends JPanel {
 	private static final long serialVersionUID = -6744712728807455322L;
+
 	private static int[] zailtasunaAukeratu(String zailtasuna) {
 		switch (zailtasuna.toLowerCase()) {
 		case "erreza":
